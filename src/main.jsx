12610cc2428f48cd35a576d8ea7946c21a1b9556
Tony Aldervall <tony.aldervall@iths.se>
2024-10-04 11:02:25 +0200

    Installed React Router and set it up in main.jsx.

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,26 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import App from './App.jsx'
 import './index.css'
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  // {
+  //   path: "/category/:categoryId",
+  //   element: <Category />,
+  // },
+  // {
+  //   path: "/recipe/:recipeId",
+  //   element: <Recipe />,
+  // }
+]);
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </StrictMode>,
 )
