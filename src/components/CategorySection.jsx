0118c94648d0f2fd36d825903b886bfe7be5eee0
Kayuta96 <serafu5546@hotmail.com>
2024-10-17 01:30:41 +0200

    Ändrade categorysection jsx och css så att det visar antal ingredienser och tid det tar för varsin recept.

--- src/components/CategorySection.jsx
+++ src/components/CategorySection.jsx
@@ -3,8 +3,6 @@ import { fetchData } from '../utils/fetch';
 import { Link } from 'react-router-dom';
 import '../styles/category-section.css';
 
-
-
 const CategorySection = ({ category }) => {
     const [filteredData, setFilteredData] = useState([]);
 
@@ -15,36 +13,37 @@ const CategorySection = ({ category }) => {
             const filtered = result.filter(item => item.categories.includes(category));
             setFilteredData(filtered);
         }
-            
+        
         getData();
     }, [category]);
 
-  return (
-    <div className="category-section">
+    return (
+        <div className="category-section">
+            <div className="category-header" id={`category-${category}`}>
+                <h2 className='category-title'>{category}drinkar</h2>
+                <Link to={`/category/${category}`} className='category-link'>Visa fler...</Link>
+            </div>
 
-      <div className="category-header" id={`category-${category}`}>
-        <h2 className='category-title'>{category}drinkar</h2>
-        <Link to={`/category/${category}`} className='category-link'>Visa fler...</Link>
-      </div>
-
-      <div className="drink-grid">
-        {filteredData.slice(0, 3).map((drink, index) => (
-        <div className='drink-card' key={index}>
-
-            <Link to={`/recipe/${drink._id}`} className='drink-link'>
-                <div className='drink-image-container'>
-                    <img src={drink.imageUrl} alt={`Picture for ${drink.title}`} />
-                </div>
-
-                <div className='drink-details-container'>
-                    <h2 className='drink-title'>{drink.title}</h2>
-                </div>
-            </Link>
+            <div className="drink-grid">
+                {filteredData.slice(0, 3).map((drink, index) => (
+                    <div className='drink-card' key={index}>
+                        <Link to={`/recipe/${drink._id}`} className='drink-link'>
+                            <div className='drink-image-container'>
+                                <img src={drink.imageUrl} alt={`Picture for ${drink.title}`} />
+                            </div>
+                            <div className='drink-details-container'>
+                                <h2 className='drink-title'>{drink.title}</h2>
+                            </div>
+                            <div className="drink-extra-details">
+                                <p>Antal tid: {drink.timeInMins} minuter</p>
+                                <p>Ingredienser: {drink.ingredients.length}</p>
+                            </div>
+                        </Link>
+                    </div>
+                ))}
+            </div>
         </div>
-        ))}
-      </div>
-    </div>
-  );
+    );
 };
 
 export default CategorySection;
