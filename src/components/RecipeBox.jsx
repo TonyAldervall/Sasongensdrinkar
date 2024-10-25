d54c82bfa6affadaa453df943d00f88c0ef855b6
Kayuta96 <serafu5546@hotmail.com>
2024-10-25 09:52:20 +0200

    Recepttiteln är klart och nu displayar en box som visar bilden descriptionon ,etc

---
+++ src/components/RecipeBox.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import Difficulty from './Difficulty';
+import TimeIcon from './TimeIcon';
+import RatingSection from './Rating';
+import '../styles/RecipeBox.css';
+
+const RecipeBox = ({ recipeData }) => {
+    if (!recipeData) return null;
+
+    return (
+        <div className='recipe-box'>
+            <div className='recipe-image-container'>
+                <img src={recipeData.imageUrl} alt={`Picture of ${recipeData.title}`} />
+            </div>
+
+            <div className='recipe-details-container'>
+                <h1>{recipeData.title}</h1>
+                <p>{recipeData.description}</p>
+
+                <div className='recipe-icon-info-div'>
+                    <span><TimeIcon /> {recipeData.timeInMins} min</span>
+                    <Difficulty
+                        nrIngredients={recipeData.ingredients.length}
+                        nrInstructions={recipeData.instructions.length}
+                    />
+                </div>
+
+                <RatingSection rating={recipeData.ratings?.length > 0 ? recipeData.ratings.reduce((a, b) => a + b) / recipeData.ratings.length : 0} readOnly={true} />
+            </div>
+        </div>
+    );
+};
+
+export default RecipeBox;
--- src/pages/Recipe.jsx
+++ src/pages/Recipe.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import Ingredients from '../components/Ingredients';
 import Navbar from '../components/Navbar';
@@ -6,23 +6,42 @@ import Instructions from '../components/Instructions';
 import '../styles/Recipe.css';
 import RatingSection from '../components/Rating';
 import Comments from '../components/CommentSection.jsx';
+import RecipeBox from '../components/RecipeBox.jsx';
+import { fetchData } from '../utils/fetch';
 
 function Recipe() {
     const { recipeId } = useParams();
+    const [recipeData, setRecipeData] = useState(null);
+
+    useEffect(() => {
+        const fetchRecipeData = async () => {
+            const data = await fetchData(`https://recept5-kivel.reky.se/recipes/${recipeId}`);
+            setRecipeData(data);
+        };
+
+        if (recipeId) { 
+            fetchRecipeData();
+        }
+    }, [recipeId]);
 
     return (
         <>
             <Navbar />
-            <h1>Hello {recipeId}!</h1>
-            <RatingSection />
-            <div className='recipeContainer'>
-                <Ingredients />
-                <Instructions />
-            </div>
+            {recipeData && (
+                <>
+                    <RecipeBox recipeData={recipeData} />
+                    <RatingSection rating={recipeData.ratings?.length > 0 ? recipeData.ratings.reduce((a, b) => a + b) / recipeData.ratings.length : 0} readOnly={true} />
 
-            <div className="comments-wrapper"> 
-                <Comments recipeId={recipeId} />
-            </div>
+                    <div className='recipeContainer'>
+                        <Ingredients ingredients={recipeData.ingredients} />
+                        <Instructions instructions={recipeData.instructions} />
+                    </div>
+
+                    <div className="comments-wrapper"> 
+                        <Comments recipeId={recipeId} />
+                    </div>
+                </>
+            )}
         </>
     );
 }
